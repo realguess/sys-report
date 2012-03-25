a60cfe423dee39cae951793457491568a64e1ab0
Chao Huang <chao@realguess.net>
2012-03-24 21:27:42 -0400

    Added initial system report script.

---
+++ sys-report.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+##
+# Print system information, used for keeping system up to date.
+#
+# For Amazon AWS use only.
+# 
+# Chao Huang (chao@realguess.net)
+# March 24, 2012
+##
+
+# System information
+uname -a
+echo ''
+
+# File system disk space
+df -aTh | grep '^Filesystem\|^/dev/'
+echo ''
+
+# Nginx
+nginxv -v
+echo ''
+
+# MongoDB
+mongo --version
+echo ''
+
+# Node packages
+npm outdated -g
+echo ''
